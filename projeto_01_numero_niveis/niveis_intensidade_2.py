5916aab0a7457f4c9645fe0deb6a2d8a247b0f74
Willian Alves <wiilliian.alves@gmail.com>
2017-09-02 19:13:30 -0400

    primeiro projeto nivel de intesidade

---
+++ projeto_01_numero_niveis/niveis_intensidade_2.py
@@ -0,0 +1,62 @@
+'''
+    Escreva um programa de computador capaz de reduzir o número de níveis de intensidade
+    em uma imagem de 256 a 2 em incrementos inteiros de potencia de 2. O número de níveis
+    de intensidade deve ser uma variavel de entrada do seu programa
+'''
+
+from PIL import Image
+
+#Entrada do numero
+number = int(input('Digite Aqui a quantidade de Bits'))
+
+#path da imagem
+src_image = "../../img/Fig0221.tif"
+
+#Numero de Bits
+K = number
+
+#niveis de cinza
+Nc = 2 ** K
+
+#Imagem de entrada
+in_img = Image.open(src_image)
+
+#Salvar imagem de entrada
+in_img.save('in_image.png', 'png')
+
+#mostra imagem de entrada
+in_img.show()
+
+#carrega na variavel a imagem de entrada
+in_pixel = in_img.load()
+
+# Tamanho da imagem LINHAS, COLUNAS
+M, N = in_img.size
+
+#Imagem de saida
+out_img = Image.new("L", (M,N))
+out_pixel = out_img.load()
+
+menor = 255
+maior = 0
+
+for m in range(M):
+    for n in range(N):
+        out_pixel[m,n] = round((Nc/256)*in_pixel[m,n])
+
+for m in range(M):
+    for n in range(N):
+        if menor > out_pixel[m,n]:
+            menor = out_pixel[m,n]
+        if maior < out_pixel[m,n]:
+            maior = out_pixel[m,n]
+
+
+for m in range(M):
+    for n in range(N):
+        out_pixel[m,n] = round(255 * ((out_pixel[m, n] - menor)/(maior - menor)))
+
+out_img.save('foto'+ str(number) + '.png', 'png')
+
+#mostra imagem de saida
+out_img.show()
