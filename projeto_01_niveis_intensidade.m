44961780e1a8e0d2f45390537daa828e2e94414d
willalves <wiilliian.alves@gmail.com>
2017-09-03 16:34:47 -0300

    adicionado arquivos matlab

---
+++ projeto_01_niveis_intensidade.m
@@ -0,0 +1,38 @@
+% Escreva um programa de computador capaz de reduzir o número de niveis de
+% intensidade emu ma imagem de 256 a 2 em incrementos inteiros de potencia de 2.
+% O número de níveis de intensidade deve ser uma variável de entrada do seu programa
+
+
+% Caminho do arquivo que deverá ser modificado
+path = './img/Fig0221.tif';
+
+% Novo nível de cinza da imagem 
+% K é o número de bits, para imagens bnárias inserir 0
+% K = 0;
+
+k = input('Digite o numero de bits');
+Nc = 2 .^ k;
+
+% Imagem de entrada 
+I = imread(path);
+
+%Extraindo o número de linhas(M) e colunas(N) da imagem
+[M, N] = size(I);
+
+imshow(I,[]);
+title('Imagem original')
+
+%Criando uma imagem de saida do mesmo tamanho da de entrada
+I_out = zeros(M,N);
+
+%Calculando os novos níveis de cinza para imagem
+for m = 1:M
+    for n = 1:N
+        I_out(m,n) = (Nc/255)*I(m,n);
+    end
+end
+
+imwrite(I_out, 'matlab_imagem_8.png')
+
+figure();
+imshow(I_out,[]);
