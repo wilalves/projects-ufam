cd01efd301aba713503f70fe554fe45d434b4a3b
Willian Alves <wiilliian.alves@gmail.com>
2017-09-05 15:10:17 -0400

    arrumar ampliar uma imagem

--- python_projeto_02_ampliar_reduzir_replicacao_pixel/projeto_02_ampliar.py
+++ python_projeto_02_ampliar_reduzir_replicacao_pixel/projeto_02_ampliar.py
@@ -6,7 +6,7 @@
 from PIL import Image
 
 # Path da imagem
-src_image = "../img/Fig0219.tif"
+src_image = "out_image_512_python.png"
 
 # Imagem de entrada
 in_img = Image.open(src_image)
@@ -31,26 +31,24 @@ l = 1
 out_img = Image.new("L", (MM,NN))
 out_pixel = out_img.load()
 
-for m in range(MM):
-    for n in range(NN):
-
+for m in range(1, MM):
+    for n in range(1, NN):
         out_pixel[m,n] = in_pixel[k,l]
         l = l + 1
     l = 1
     k = k + 1
 
-for m in range(MM):
+for m in range(1, MM):
     for n in range(2, NN):
         out_pixel[m,n] = in_pixel[m,n-1]
 
-for m in range(MM):
+for m in range(1, MM):
     for n in range(2, NN):
         out_pixel[m,n] = in_pixel[m-1,n]
 
 for m in range(2, MM):
     for n in range(2, NN):
         out_pixel[m,n] = in_pixel[m,n-1]
-
 
 out_img.save('out_image_python.png', 'png')
 
