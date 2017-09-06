5f8ecf9ea65034ee52b3f5050185b39aad988bec
willalves <wiilliian.alves@gmail.com>
2017-09-06 01:26:33 -0300

    added arquivo matlab

--- matlab_projeto_02_ampliar_reduzir_ replicação_pixel/projeto_02_ampliar.m
+++ matlab_projeto_02_ampliar_reduzir_ replicação_pixel/projeto_02_ampliar.m
@@ -11,43 +11,17 @@ I = imread(path);
 % Inteiro de entrada
 f = 2;
 
-M1 = M * f;
-N1 = N * f;
-
-k=1;
-l=1;
-
-% Criando uma imagem de saida
-I_out = zeros(M1, N1);
-
-for i=1:f:M1
-   for j=1:f:N1
-       I_out(i,j)= I(k,l);
-       l=l+1;
-   end
-   l=1;
-   k=k+1;
+for c=1:1:N
+    AA(:,f*c) = I(:,c);
+    AA(:,(f*c-1)) = AA(:,f*c);
+end
+    
+% Replicando linhas:
+for c=1:1:M
+    BB(f*c,:)= AA(c,:);
+    BB((f*c-1),:)= BB(f*c,:);
 end
 
-for i=1:f:M1
-   for j=2:f:N1
-       I_out(i,j)=I_out(i,j-1);
-   end
-end
-
-for j=1:f:M1
-    for i=2:f:N1
-        I_out(i,j)=I_out(i-1,j);
-    end
-end
-
-for i=2:f:M1
-    for j=2:f:N1
-        I_out(i,j)= I_out(i,j-1);
-    end
-end
-
-imshow(I, []);
 figure;
-imshow(I_out, []);
-imwrite(I_out,'Fig0219_zoom_1024).tif');
+imshow(BB, []);
+imwrite(BB,'Fig0219_zoom_1024).tif');
