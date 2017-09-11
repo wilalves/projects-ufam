941b15959c36c8d4fe254620685e7abae3256402
Willian Alves <wiilliian.alves@gmail.com>
2017-09-10 20:47:18 -0400

    add python de potencia

---
+++ python_projeto_04_transformacao_potencia_log/projeto_04_transformacao_potencia.py
@@ -0,0 +1,32 @@
+import math
+from PIL import Image
+
+# Entrada do numero
+number = int(input('Digite Aqui a quantidade de Bits'))
+lambd =  int(input('Digite Aqui a quantidade de Bits'))
+
+# Path da imagem
+src_img = '../img/Fig0308.tif'
+
+# Imagem de entrada
+in_img = Image.open(src_img)
+
+
+[M, N] = in_img.size
+
+# Imagem de saida
+out_img = Image.new("L", (M,N))
+out_pixel = out_img.load()
+
+# Carrega na variavel a imagem de entrada
+in_pixel = in_img.load()
+
+#Calculando os n�veis de cinza para imagem
+for m in range(M):
+    for n in range(N):
+        out_pixel[m,n] = round(number*((in_pixel[m,n]) ** lambd))
+
+
+out_img.save('out_image_python-'+ str(number) + '.png', 'png')
+
+out_img.show()
