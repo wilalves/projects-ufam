121dae80c2c09d82375b0847e77cbfb6ad78e96d
Willian Alves <wiilliian.alves@gmail.com>
2017-09-14 19:57:56 -0400

    continua mais um pouquinho na interpolação bilinear

--- python_projeto_03_ampliar_reduzir_interpolacao_bilinear/interpolacao_bilinear.py
+++ python_projeto_03_ampliar_reduzir_interpolacao_bilinear/interpolacao_bilinear.py
@@ -3,12 +3,29 @@ from PIL import Image
 # def interpolacao_bilinear (self, src_image, resolucao):
 
     # Imagem de entrada
-src_image = "../img/Fig0219.tif"
+src_image = "../img/Fig0220a.tif"
 in_img = Image.open(src_image)
 
 M, N = in_img.size
 
-resolucao_in = in_img.info['resolution']
+resolucao_in = in_img.info['dpi']
 
-print(resolucao_in)
+MM = (M*100/resolucao_in)
+NN = (N*100/resolucao_in)
+
+# Imagem de saida
+out_img_MM = Image.new("L", (M,N))
+out_pixel_MM = out_img_MM.load()
+
+out_img_NN = Image.new("L", (M,N))
+out_pixel_NN = out_img_NN.load()
+
+
+for m in range(MM):
+    out_img_MM[m] = M - (M - 1)* (MM - out_img_MM[m]) / MM - 1
+
+for m in range(MM):
+    out_img_NN[n] = N - (N - 1)* (NN - out_img_NN[n]) / NN - 1
+
+
 
