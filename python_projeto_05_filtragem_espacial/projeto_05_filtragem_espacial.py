0f86ae83250138ee00e9072a80c96c3b3e4a2314
Willian Alves <wiilliian.alves@gmail.com>
2017-10-20 16:26:28 -0400

    add mais um pouquinho de codigo

--- python_projeto_05_filtragem_espacial/projeto_05_filtragem_espacial.py
+++ python_projeto_05_filtragem_espacial/projeto_05_filtragem_espacial.py
@@ -9,7 +9,9 @@ def meu_filtro(imagem, mascara):
     out_img = Image.new("L", (M + 2, N + 2))
     out_pixel = out_img.load()
 
-    print(out_img)
+    mascara = Image.new("L", (3, 3))
+    mascara_M, mascara_N = mascara.size
+    mask = mascara.load()
 
     for m in range(M):
         for n in range(N):
@@ -28,16 +30,26 @@ def meu_filtro(imagem, mascara):
     for a in range(0, M+1):
         out_pixel[a, N+1] = out_pixel[a, N]
 
+    # Faz o a varredura da imagem aplicando o filtro
+    for m in range(1, M):
+        for n in range(1, N):
+            for k in range(1, mascara_M):
+                for l in range(1, mascara_N):
+                    mask[m, n] = mask[m, n] + w(k, l) * Ip(i + (k - 1), j + (l - 1));
+                end
+            end
+            Ifilt(i, j) = round(Ifilt(i, j));
+        end
+    end
+
+
     print("aqui")
 
 # Imagem de entrada
 in_img = Image.open("../img/teste.tif")
 
-mascara = Image.new("L", (3, 3))
-mascara_M, mascara_N = mascara.size
-mask = mascara.load()
-
 meu_filtro(in_img, mascara)
+
 
 
 #mostra imagem de entrada
